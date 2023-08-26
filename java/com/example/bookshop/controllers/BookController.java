9f80d32ab8eafc2cbae0205f8a798b5346e9e192
Dung289 <115990370+Dung289@users.noreply.github.com>
2023-08-26 15:00:36 +0700

    Update BookController.java

--- java/com/example/bookshop/controllers/BookController.java
+++ java/com/example/bookshop/controllers/BookController.java
@@ -4,12 +4,13 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -18,12 +19,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.example.bookshop.dtos.BookDTO;
+import com.example.bookshop.dtos.ReviewDTO;
 import com.example.bookshop.models.Book;
+import com.example.bookshop.models.Review;
+import com.example.bookshop.models.User;
 import com.example.bookshop.serviceImpl.BookServiceImpl;
 import com.example.bookshop.serviceImpl.CategoryServiceImpl;
+import com.example.bookshop.serviceImpl.ReviewServiceImpl;
+import com.example.bookshop.serviceImpl.UserServiceImpl;
 
 @Controller
 @RequestMapping("/book")
@@ -36,15 +43,28 @@ public class BookController {
 	@Autowired
 	CategoryServiceImpl categoryServiceImpl;
 	
+	@Autowired
+	UserServiceImpl userServiceImpl;
+	
+	@Autowired
+	ReviewServiceImpl reviewServiceImpl;
+	
 	@GetMapping("/")
-	private String getAllBooks(Model model) {
+	private String getAllBooks(Model model, @SessionAttribute(required = false) User user) {
 		model.addAttribute("books", bookServiceImpl.getAllBooks());
-//		return "bookTable";
-		return "bookList";
+		try {
+			String userRole = user.getRole();
+		if (userRole.compareTo("user") == 0) 
+			return "bookList";
+		else 
+			return "bookTable";
+		} catch (Exception e) {
+			return "bookList";
+		}
 	}
 	
 	@GetMapping("/{id}")
-	private String getBookById(Model model, @PathVariable Long id) {
+	private String getBookById(Model model, @PathVariable Long id, @SessionAttribute(required = false) User user) {
 		Optional<Book> opBook = bookServiceImpl.getBookById(id);
 		if (opBook.isPresent()) {
 			Book book = opBook.get();
@@ -58,15 +78,35 @@ public class BookController {
 			bookDTO.setPrice(book.getPrice());
 			bookDTO.setQuantitySold(book.getQuantitySold());
 			bookDTO.setImageName(book.getImageName());
-			
 			model.addAttribute("bookDTO", bookDTO);
 		} else {
 			model.addAttribute("bookDTO", new BookDTO());
 		}
 		model.addAttribute("categories", categoryServiceImpl.getAllCategories());
 		model.addAttribute("id", id);
-//		return "bookForm";
-		return "bookDetail";
+		List<Review> reviews = reviewServiceImpl.getAllByBook(opBook.get());
+		List<ReviewDTO> reviewDTOs = new ArrayList<ReviewDTO>();
+		
+		for(Review review : reviews) {
+			ReviewDTO reviewDTO = new ReviewDTO();
+			reviewDTO.setId(review.getId());
+			reviewDTO.setUserFullname(review.getUser().getFirstName() + " " + review.getUser().getLastName());
+			reviewDTO.setUserImage(review.getUser().getImageName());
+			reviewDTO.setRate(review.getRate());
+			reviewDTO.setComment(review.getComment());
+			reviewDTOs.add(reviewDTO);
+		}
+		model.addAttribute("reviewDTOs", reviewDTOs);
+		try {
+			String userRole = user.getRole();
+			if (userRole.compareTo("user") == 0)
+				return "bookDetail";
+			else
+				return "bookForm";
+		} catch (Exception e) {
+			return "bookDetail";
+		}
+		
 	}
 	
 	@PostMapping("/save/")
