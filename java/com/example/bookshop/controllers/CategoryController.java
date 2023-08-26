04e86751efa7e904b81897837bd5eebb3f96cdd1
Dung289 <115990370+Dung289@users.noreply.github.com>
2023-08-26 15:01:12 +0700

    Update CategoryController.java

--- java/com/example/bookshop/controllers/CategoryController.java
+++ java/com/example/bookshop/controllers/CategoryController.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -54,9 +53,4 @@ public class CategoryController {
 		return "redirect:/category/";
 	}
 	
-//	@DeleteMapping("/delete/{id}")
-//	public String deleteCategory(@PathVariable int id) {
-//		categoryServiceImpl.deleteCategoryById(id);
-//		return "redirect:/category/";
-//	}
 }
